27fc80f6d01c3d143991069cb7909da7093957c0
eduardoams <eduardoa.ms14@gmail.com>
2023-10-10 08:05:04 -0300

    Primeiro commit

---
+++ src/main/java/com/softwaressilva/demospringbootmongodb/DemoSpringBootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.softwaressilva.demospringbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwaressilva/demospringbootmongodb/DemoSpringBootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwaressilva.demospringbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
