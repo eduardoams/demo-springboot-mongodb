d75bd26cdd7fca944c53f3c0b1868d79489c8888
Eduardo Souza <eduardoa.ms14@gmail.com>
2023-10-17 08:27:28 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/softwaressilva/demospringbootmongodb/resource/UserResource.java
+++ src/main/java/com/softwaressilva/demospringbootmongodb/resource/UserResource.java
@@ -39,4 +39,10 @@ public class UserResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
