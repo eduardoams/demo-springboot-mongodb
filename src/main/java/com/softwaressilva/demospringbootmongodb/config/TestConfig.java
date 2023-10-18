797a04bd613af84d66bc017abc93a02caa671275
Eduardo Souza <eduardoa.ms14@gmail.com>
2023-10-18 08:07:48 -0300

    Referência dos posts do usuário

--- src/main/java/com/softwaressilva/demospringbootmongodb/config/TestConfig.java
+++ src/main/java/com/softwaressilva/demospringbootmongodb/config/TestConfig.java
@@ -39,7 +39,15 @@ public class TestConfig implements CommandLineRunner {
 
         Post p1 = new Post(null, sdf.parse("01/09/1910"), "Vai Corinthians!", "Eternamente dentro de nossos corações", new AuthorDto(u1));
         Post p2 = new Post(null, sdf.parse("01/06/2023"), "Bom dia", "Vamos com tudo, Fiel", new AuthorDto(u2));
+        Post p3 = new Post(null, sdf.parse("15/08/2023"), "Neo Química Arena", "A Arena mais bonita do Brasil", new AuthorDto(u1));
 
-        postRepository.saveAll(Arrays.asList(p1, p2));
+        //Salvando Post, aninhando Author à coleção deste objeto
+        postRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+        //Referenciando Post à coleção do objeto User
+        u1.getPosts().addAll(Arrays.asList(p1, p3));
+        u2.getPosts().add(p2);
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
     }
 }
