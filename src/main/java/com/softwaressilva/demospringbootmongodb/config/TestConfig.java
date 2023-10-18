44d5e21ef223bcf285ac04466ddb4cc84230bbb1
Eduardo Souza <eduardoa.ms14@gmail.com>
2023-10-18 07:00:52 -0300

    Criação da entidade Post com User aninhado

--- src/main/java/com/softwaressilva/demospringbootmongodb/config/TestConfig.java
+++ src/main/java/com/softwaressilva/demospringbootmongodb/config/TestConfig.java
@@ -1,12 +1,16 @@
 package com.softwaressilva.demospringbootmongodb.config;
 
+import com.softwaressilva.demospringbootmongodb.domain.Post;
 import com.softwaressilva.demospringbootmongodb.domain.User;
+import com.softwaressilva.demospringbootmongodb.repository.PostRepository;
 import com.softwaressilva.demospringbootmongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class TestConfig implements CommandLineRunner {
@@ -14,16 +18,27 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User u1 = new User(null, "Eduardo", "eduardo@gmail.com");
         User u2 = new User(null, "Amanda", "amanda@gmail.com");
         User u3 = new User(null, "Chloe", "chloe@gmail.com");
-        User u4 = new User(null, "Spazio", "spazio@gmail.com");
 
-        userRepository.saveAll(Arrays.asList(u1, u2, u3, u4));
+        userRepository.saveAll(Arrays.asList(u1, u2, u3));
+
+        Post p1 = new Post(null, sdf.parse("01/09/1910"), "Vai Corinthians!", "Eternamente dentro de nossos corações", u1);
+        Post p2 = new Post(null, sdf.parse("01/06/2023"), "Bom dia", "Vamos com tudo, Fiel", u2);
+
+        postRepository.saveAll(Arrays.asList(p1, p2));
     }
 }
